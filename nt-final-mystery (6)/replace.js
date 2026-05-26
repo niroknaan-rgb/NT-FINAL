903236c9eb718c626df24db01aa666ed383d4db5
niroknaan-rgb <niroknaan@gmail.com>
2026-05-26 21:39:53 +0300

    Delete nt-final-mystery (6) directory

--- nt-final-mystery (6)/replace.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-code = code.replace(/hover:scale-110 transition-transform/g, 'hover:scale-110 active:scale-95 transition-transform duration-100');
-fs.writeFileSync('src/App.tsx', code);
-console.log('Replaced successfully');
