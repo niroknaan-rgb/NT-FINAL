e43c2d6f0e6f522ae985dc216505971092bff1f5
niroknaan-rgb <niroknaan@gmail.com>
2026-05-30 00:14:42 +0300

    Delete nt-final-mystery (16) directory

--- nt-final-mystery (16)/replace2.ts
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-content = content.replace(/<button[^>]*onClick={onClose}[^>]*className="absolute bottom-6 left-6 w-\[50px\] h-\[50px\][^>]*>[\s\S]*?<\/button>/g, '<ReturnButton onClick={onClose} />');
-fs.writeFileSync('src/App.tsx', content);
