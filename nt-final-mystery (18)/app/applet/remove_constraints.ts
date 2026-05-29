1fbfc30949a64c233a07aa238d329bae4b0fd2ca
niroknaan-rgb <niroknaan@gmail.com>
2026-05-30 00:35:05 +0300

    Delete nt-final-mystery (18) directory

--- nt-final-mystery (18)/app/applet/remove_constraints.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-
-// Replace w-full h-full max-w-[calc(100vh*16/9)] max-h-[calc(100vw*9/16)] with w-full h-full
-content = content.replace(/w-full h-full max-w-\[calc\(100vh\*16\/9\)\] max-h-\[calc\(100vw\*9\/16\)\]/g, 'w-full h-full');
-
-fs.writeFileSync('src/App.tsx', content);
-console.log('Removed 16:9 constraints from close-ups.');
